57956a786b99f9477b70efb2dbd1cceab293d5a9
Yogyata Singh <134354066+yogyata-04@users.noreply.github.com>
2023-07-28 12:13:26 +0530

    Update MySolution.py

--- MySolution.py
+++ MySolution.py
@@ -12,14 +12,6 @@ import cv2.aruco as aruco
 from color_identification import color_identification
 
 
-def get_node_pairs(n1, n2, both_ends=True):
-    if both_ends:
-        node_pairs = [[n1, n2], [n2, n1]]
-    else:
-        node_pairs = [[n1, n2]]
-    return node_pairs
-
-
 class Dijkstra:
     def __init__(self, edges):
         self.edges = [make_edge(*edge) for edge in edges]
@@ -32,23 +24,6 @@ class Dijkstra:
                 ([edge.start, edge.end] for edge in self.edges), []
             )
         )
-
-    def remove_edge(self, n1, n2, both_ends=True):
-        node_pairs = get_node_pairs(n1, n2, both_ends)
-        edges = self.edges[:]
-        for edge in edges:
-            if [edge.start, edge.end] in node_pairs:
-                self.edges.remove(edge)
-
-    def add_edge(self, n1, n2, cost=1, both_ends=True):
-        node_pairs = get_node_pairs(n1, n2, both_ends)
-        for edge in self.edges:
-            if [edge.start, edge.end] in node_pairs:
-                return ValueError('Edge {} {} already exists'.format(n1, n2))
-
-        self.edges.append(Edge(start=n1, end=n2, cost=cost))
-        if both_ends:
-            self.edges.append(Edge(start=n2, end=n1, cost=cost))
 
     @property
     def neighbours(self):
